556a102f6ed3dc8009c6700611ea08fc850e4366
Christophe Nouguier <christophe.nouguier@free.fr>
2019-11-08 20:23:05 +0100

    wip: Enhance dockerfiles to use krawler based image #1

--- .travis.env.sh
+++ .travis.env.sh
@@ -4,7 +4,7 @@
 if [[ -z "$TRAVIS_TAG" ]]
 then
 	export TAG=latest
-	export KRAWLER_TAG=master
+	export KRAWLER_TAG=latest
 else
 	export TAG=$(node -p -e "require('./package.json').version")
 	export KRAWLER_TAG=v$(node -p -e "require('./package.json').peerDependencies['@kalisio/krawler']")
