d46ba45be66861f7ac798d0125ba12ca7d974ff9
Christophe Nouguier <christophe.nouguier@free.fr>
2019-11-08 20:25:32 +0100

    wip: Enhance dockerfiles to use krawler based image #1

--- .travis.build.sh
+++ .travis.build.sh
@@ -4,9 +4,9 @@ source .travis.env.sh
 echo Building k-hubeau $TAG with Krawler $KRAWLER_TAG
 
 # Build Stations image
-docker build --build-arg KRAWLER_TAG=$KRAWLER_TAG -f dockerfile.stations -t kalisio/k-hubeau:stations-$TAG
+docker build --build-arg KRAWLER_TAG=$KRAWLER_TAG -f dockerfile.stations -t kalisio/k-hubeau:stations-$TAG .
 # Build Observations image
-docker build --build-arg KRAWLER_TAG=$KRAWLER_TAG -f dockerfile.observations -t kalisio/k-hubeau:observations-$TAG
+docker build --build-arg KRAWLER_TAG=$KRAWLER_TAG -f dockerfile.observations -t kalisio/k-hubeau:observations-$TAG .
 
 # Push the built images to Docker hub
 docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
