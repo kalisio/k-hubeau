c12227c02c2246769d8857cb758283b88dccb846
Christophe Nouguier <christophe.nouguier@free.fr>
2019-11-13 14:47:07 +0100

    wip: Decrease image size using multistage build #2

--- .travis.build.sh
+++ .travis.build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+
+# Source the environment to define Krawler and image versions
 source .travis.env.sh
+echo Building $IMAGE_NAME:stations-$IMAGE_TAG with Krawler-$KRAWLER_TAG
+echo Building $IMAGE_NAME:observations-$IMAGE_TAG with Krawler-$KRAWLER_TAG
 
-echo Building k-hubeau $TAG with Krawler $KRAWLER_TAG
+# Build the images
+docker build --build-arg KRAWLER_TAG=$KRAWLER_TAG -f dockerfile.stations -t $IMAGE_NAME:stations-$IMAGE_TAG .
+docker build --build-arg KRAWLER_TAG=$KRAWLER_TAG -f dockerfile.observations -t $IMAGE_NAME::observations-$IMAGE_TAG .
 
-# Build Stations image
-docker build --build-arg KRAWLER_TAG=$KRAWLER_TAG -f dockerfile.stations -t kalisio/k-hubeau:stations-$TAG .
-# Build Observations image
-docker build --build-arg KRAWLER_TAG=$KRAWLER_TAG -f dockerfile.observations -t kalisio/k-hubeau:observations-$TAG .
-
-# Push the built images to Docker hub
+# Publish the images
 docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
-docker push kalisio/k-hubeau:stations-$TAG
-docker push kalisio/k-hubeau:observations-$TAG
+docker push $IMAGE_NAME::stations-$IMAGE_TAG
+docker push $IMAGE_NAME::observations-$IMAGE_TAG
--- .travis.env.sh
+++ .travis.env.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Define image name
+IMAGE_NAME="$TRAVIS_REPO_SLUG"
+
 # Build docker with version number only on release
 if [[ -z "$TRAVIS_TAG" ]]
 then
-	export TAG=latest
+	export IMAGE_TAG=latest
 	export KRAWLER_TAG=latest
 else
-	export TAG=$(node -p -e "require('./package.json').version")
+	export IMAGE_TAG=$(node -p -e "require('./package.json').version")
 	export KRAWLER_TAG=v$(node -p -e "require('./package.json').peerDependencies['@kalisio/krawler']")
 fi
