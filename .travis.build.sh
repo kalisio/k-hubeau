3a9edb74701c34629a98af84954bf375c085b095
Christophe Nouguier <christophe.nouguier@free.fr>
2019-11-13 14:52:34 +0100

    wip: Decrease image size using multistage build #2

--- .travis.build.sh
+++ .travis.build.sh
@@ -7,9 +7,9 @@ echo Building $IMAGE_NAME:observations-$IMAGE_TAG with Krawler-$KRAWLER_TAG
 
 # Build the images
 docker build --build-arg KRAWLER_TAG=$KRAWLER_TAG -f dockerfile.stations -t $IMAGE_NAME:stations-$IMAGE_TAG .
-docker build --build-arg KRAWLER_TAG=$KRAWLER_TAG -f dockerfile.observations -t $IMAGE_NAME::observations-$IMAGE_TAG .
+docker build --build-arg KRAWLER_TAG=$KRAWLER_TAG -f dockerfile.observations -t $IMAGE_NAME:observations-$IMAGE_TAG .
 
 # Publish the images
 docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
-docker push $IMAGE_NAME::stations-$IMAGE_TAG
-docker push $IMAGE_NAME::observations-$IMAGE_TAG
+docker push $IMAGE_NAME:stations-$IMAGE_TAG
+docker push $IMAGE_NAME:observations-$IMAGE_TAG
