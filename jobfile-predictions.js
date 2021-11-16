a724d55036d1c1a80af29bbba1229b00c01fdb78
Luc Claustres <luc.claustres@orange.fr>
2021-11-16 18:47:11 +0100

    wip: Integrate predictions based on machine learning models - updated predictions job to use models path  (#22)

--- jobfile-predictions.js
+++ jobfile-predictions.js
@@ -8,7 +8,7 @@ const _ = require('lodash')
 // Configuration
 const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/kano'
 const baseUrl = process.env.PREDIKT_URL || 'http://localhost:5000/predict'
-const modelsPath = process.env.MODELS_PATH || path.join('..', 'predikt', 'models', 'output', 'water_level_rnn', 'multiple', '12H')
+const modelsPath = process.env.PREDIKT_MODELS_PATH || path.join('..', 'predikt', 'models', 'output', 'water_level_rnn', 'multiple', '12H')
 const ttl = parseInt(process.env.TTL) || (7 * 24 * 60 * 60)  // duration in seconds
 const timeout = parseInt(process.env.TIMEOUT) || (30 * 60 * 1000) // duration in miliseconds
 const collection = 'hubeau-observations'
